c9e4a73814a1c63258f1fb8fe7c3159cdeea8450
James <auminist3@gmail.com>
2025-06-05 18:55:03 +1200

    feat: added player class and draw calls to render player on game window

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    deltaTime = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
+# Main Game Loop
     while True:
         for event in pygame.event.get():
             if event == pygame.QUIT:
                 return
             
         screen.fill(color="black")
+        time_since_last_call = game_clock.tick(60)
+        deltaTime = time_since_last_call/1000
+
+        player.draw(screen)
+
         pygame.display.flip()
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 
 # Ensures program can only be executed by running this file directly, not as an import.
