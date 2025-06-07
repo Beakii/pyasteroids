d0fea72e43361b0e71597a0c44702a2d2c9da6c9
James <auminist3@gmail.com>
2025-06-07 12:43:37 +1200

    feat: added asteroid split mechanic when shot

--- asteroid.py
+++ asteroid.py
@@ -1,8 +1,27 @@
 from circle import *
+from constants import *
+import random
 
 class Asteroid(Circle):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            angle = random.uniform(20, 50)
+            random_angle_1 = self.velocity.rotate(angle)
+            random_angle_2 = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+            asteroid_1.velocity = random_angle_1 * 1.2
+            asteroid_2.velocity = random_angle_2 * 1.2
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
